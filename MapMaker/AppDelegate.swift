efa9a1028c5ccfc5ba434f1f45d457f3496f2d6c
Cameron Martin <cameronmartin@davids-macbook-air-3.local>
2017-02-27 17:26:17 -0500

    let's see if this works now

--- MapMaker/AppDelegate.swift
+++ MapMaker/AppDelegate.swift
@@ -15,7 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     //changes
     //more changes
-    //changed
+    //changed again
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
